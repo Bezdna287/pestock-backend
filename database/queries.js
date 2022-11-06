8bc222dbcc9b1b9c1d01fc0573ffb55c9cb068c2
nilodude <alphone@protonmail.com>
2022-11-06 18:59:50 +0100

    more basic API functions

--- database/queries.js
+++ database/queries.js
@@ -16,7 +16,7 @@ const getImages = (request, response) => {
   })
 }
 
-const getImagesById = (request, response) => {
+const getImageById = (request, response) => {
   const id = parseInt(request.params.id)
 
   pool.query('SELECT * FROM images WHERE id = $1', [id], (error, results) => {
@@ -27,7 +27,42 @@ const getImagesById = (request, response) => {
   })
 }
 
+const getImagesByCollection = (request, response) => {
+  const idCollection = parseInt(request.params.idCollection)
+
+  pool.query('SELECT * FROM images WHERE id_collection = $1', [idCollection], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+const getCollections = (request, response) => {
+  pool.query('SELECT * FROM collections ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+const getCollectionById = (request, response) => {
+  const idCollection = parseInt(request.params.idCollection)
+
+  pool.query('SELECT * FROM collections WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+
 module.exports = {
   getImages,
-  getImagesById
+  getImageById,
+  getCollections,
+  getCollectionById,
+  getImagesByCollection
 }
--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
+const apiName = 'pestock'
 const apiPort = 3000
 const db = require('./database/queries')
+
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
@@ -15,9 +17,15 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.get('/images', db.getImages)
+app.get(apiName+'/images', db.getImages)
 
-app.get('/images/:id', db.getImagesById)
+app.get(apiName+'/images/:id', db.getImageById)
+
+app.get(apiName+'/collections', db.getCollections)
+
+app.get(apiName+'/collections/:idCollection', db.getCollectionById)
+
+app.get(apiName+'/collection/:idCollection', db.getImagesByCollection)
 
 app.get('/daigua', (req, res) => {
     res.send('TEDICHOKEDAIWA')
