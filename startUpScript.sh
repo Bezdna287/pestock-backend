79a30bf29f0283991eb041e061d0108e6b6afe9c
nilodude <alphone@protonmail.com>
2023-03-06 15:43:31 +0100

    docker compose stable. cleanup previous config. need to setup nginx

--- startUpScript.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-su -l postgres -c /usr/pgsql-15/bin/initdb
-su -l postgres -c "/usr/pgsql-15/bin/pg_ctl -D /var/lib/pgsql/15/data -l /tmp/pg_logfile start"
-cat eldump.sql | psql -U postgres
