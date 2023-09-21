316f8796fdc24d231edfee7879029922dad95d20
nilodude <alphone@protonmail.com>
2023-09-21 16:05:51 +0200

    dumBD without --clean --if-exists

--- dumpBD.zsh
+++ dumpBD.zsh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 mv currentDump/* dumps/eldump.sql.bak
 
-sudo docker exec -t laputabasededatos pg_dumpall -c --if-exists -U postgres > currentDump/dump`date +%d%m%Y"_"%H%M%S`.sql
+sudo docker exec -t laputabasededatos pg_dumpall -U postgres > currentDump/dump`date +%d%m%Y"_"%H%M%S`.sql
