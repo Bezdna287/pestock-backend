719cd16c1620009665896944604b26e32b1e8d5f
nilodude <alphone@protonmail.com>
2023-09-21 18:15:53 +0200

    fix destroy script

--- destroy.zsh
+++ destroy.zsh
@@ -1,16 +1,5 @@
 #!/bin/zsh
 
-# stop all containers
-sudo docker stop $(sudo docker ps -a -q)
-
-# remove all containers except DB
-sudo docker rm pstock-frontend
-sudo docker rm pstock-backend
-
-# remove all images except DB
-sudo docker rmi pstock-frontend
-sudo docker rmi pstock-backend
-
 mv currentDump/* dumps/eldump.sql.bak
 
 sudo docker exec -t laputabasededatos pg_dumpall -U postgres > currentDump/dump`date +%d%m%Y"_"%H%M%S`.sql
@@ -19,5 +8,20 @@ python fix.py
 
 sudo docker stop laputabasededatos
 
+sudo docker stop laputabasededatos
+
 # must have dumped BD before removing container, data will be lost
 sudo docker rm laputabasededatos
+
+# stop all containers
+sudo docker stop pstock-frontend
+sudo docker stop pstock-backend
+
+# remove all containers 
+sudo docker rm pstock-frontend
+sudo docker rm pstock-backend
+
+# remove all images
+sudo docker rmi pstock-frontend
+sudo docker rmi pstock-backend
+
